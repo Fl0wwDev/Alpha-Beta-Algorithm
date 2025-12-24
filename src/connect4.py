c41137ceb177e3f75b7519bbefe6f5717193e571
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-12-24 10:38:45 +0100

    refractor

---
+++ src/connect4.py
@@ -0,0 +1,70 @@
+from board import Board
+from threading import Thread
+from queue import Queue
+from ui import information, combobox_player1, combobox_player2, row_width, window
+
+class Connect4:
+
+    def __init__(self):
+        self.board = Board()
+        self.human_turn = False
+        self.turn = 1
+        self.players = (0, 0)
+        self.ai_move = Queue()
+
+    def current_player(self):
+        return 2 - (self.turn % 2)
+
+    def launch(self):
+        self.board.reinit()
+        self.turn = 0
+        information['fg'] = 'black'
+        information['text'] = "Turn " + str(self.turn) + " - Player " + str(
+            self.current_player()) + " is playing"
+        self.human_turn = False
+        self.players = (combobox_player1.current(), combobox_player2.current())
+        self.handle_turn()
+
+    def move(self, column):
+        if not self.board.column_filled(column):
+            self.board.add_disk(column, self.current_player())
+            self.handle_turn()
+
+    def click(self, event):
+        if self.human_turn:
+            column = event.x // row_width
+            self.move(column)
+
+    def ai_turn(self, ai_level):
+        if ai_level == 1:
+            # Minimax (index 1 in combobox)
+            Thread(target=minimax_decision, args=(self.board, self.turn, ai_level, self.ai_move, self.current_player(),)).start()
+        else:
+            # Alpha-beta (index 2+ in combobox)
+            Thread(target=alpha_beta_decision, args=(self.board, self.turn, ai_level - 1, self.ai_move, self.current_player(),)).start()
+        self.ai_wait_for_move()
+
+    def ai_wait_for_move(self):
+        if not self.ai_move.empty():
+            self.move(self.ai_move.get())
+        else:
+            window.after(100, self.ai_wait_for_move)
+
+    def handle_turn(self):
+        self.human_turn = False
+        if self.board.check_victory():
+            information['fg'] = 'red'
+            information['text'] = "Player " + str(self.current_player()) + " wins !"
+            return
+        elif self.turn >= 42:
+            information['fg'] = 'red'
+            information['text'] = "This a draw !"
+            return
+        self.turn = self.turn + 1
+        information['text'] = "Turn " + str(self.turn) + " - Player " + str(
+            self.current_player()) + " is playing"
+        if self.players[self.current_player() - 1] != 0:
+            self.human_turn = False
+            self.ai_turn(self.players[self.current_player() - 1])
+        else:
+            self.human_turn = True
---
+++ src/minimax.py
@@ -0,0 +1,135 @@
+from ui import Board
+from queue import Queue
+
+class MinMax:
+
+    def __init__(self, board: Board, turn: int, ai_level: int, queue: Queue, max_player: bool):
+        self._board = board
+        self._turn = turn
+        self._ai_level = ai_level
+        self._queue = queue
+        self._max_player = max_player
+
+    @property
+    def board(self) -> Board:
+        return self._board
+    
+    @board.setter
+    def board(self, board: Board):
+        self._board = board 
+
+    @property
+    def turn(self) -> int:
+        return self._turn
+    
+    @turn.setter
+    def turn(self, turn: int):
+        self._turn = turn
+
+    @property
+    def ai_level(self) -> int:
+        return self._ai_level
+    
+    @ai_level.setter
+    def ai_level(self, ai_level: int):
+        self._ai_level = ai_level
+    
+    @property
+    def queue(self) -> Queue:
+        return self._queue
+    
+    @queue.setter
+    def queue(self, queue: Queue):
+        self._queue = queue
+
+    @property
+    def max_player(self) -> bool:
+        return self._max_player
+    
+    @max_player.setter
+    def max_player(self, max_player: bool):
+        self._max_player = max_player
+    
+    def max_value(self, board, turn, depth, max_depth):
+        if board.check_victory():
+            return -1
+        if turn > 42:
+            return 0
+        if depth >= max_depth:
+            return 0
+        possible_moves = board.get_possible_moves()
+        best_value = -2
+        for move in possible_moves:
+            updated_board = board.copy()
+            updated_board.add_disk(move, turn % 2 + 1, update_display=False)
+            value = self.min_value(updated_board, turn + 1, depth + 1, max_depth)
+            if value > best_value:
+                best_value = value
+        return best_value
+
+    def min_value(self, board, turn, depth, max_depth):
+        if board.check_victory():
+            return 1
+        if turn > 42:
+            return 0
+        if depth >= max_depth:
+            return 0
+        possible_moves = board.get_possible_moves()
+        worst_value = 2
+        for move in possible_moves:
+            updated_board = board.copy()
+            updated_board.add_disk(move, turn % 2 + 1, update_display=False)
+            value = self.max_value(updated_board, turn + 1, depth + 1, max_depth)
+            if value < worst_value:
+                worst_value = value
+        return worst_value
+
+
+    def minimax_decision(self, board, turn, ai_level, queue, max_player):
+        max_depth = 4 
+        possible_moves = board.get_possible_moves()
+        best_move = possible_moves[0]
+        best_value = -2
+        for move in possible_moves:
+            updated_board = board.copy()
+            updated_board.add_disk(move, turn % 2 + 1, update_display=False)
+            value = self.min_value(updated_board, turn + 1, 1, max_depth)
+            if value > best_value:
+                best_value = value
+                best_move = move
+        queue.put(best_move)
+
+
+    def max_value_ab(self, board, turn, alpha, beta):
+        if board.check_victory():
+            return -1
+        if turn > 42:
+            return 0
+        possible_moves = board.get_possible_moves()
+        value = -2
+        for move in possible_moves:
+            updated_board = board.copy()
+            updated_board.add_disk(move, turn % 2 + 1, update_display=False)
+            value = max(value, self.min_value_ab(updated_board, turn + 1, alpha, beta))
+            if value >= beta:
+                return value
+            alpha = max(alpha, value)
+        return value
+
+
+    def min_value_ab(self, board, turn, alpha, beta):
+        if board.check_victory():
+            return 1
+        if turn > 42:
+            return 0
+        possible_moves = board.get_possible_moves()
+        value = 2
+        for move in possible_moves:
+            updated_board = board.copy()
+            updated_board.add_disk(move, turn % 2 + 1, update_display=False)
+            value = min(value, self.max_value_ab(updated_board, turn + 1, alpha, beta))
+            if value <= alpha:
+                return value
+            beta = min(beta, value)
+        return value
+
