b2fb4aa7faa33f8cd6846d883e84b670cbb6fd55
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-12-27 11:38:30 +0100

    chore: remove unused alpha beta

--- src/minmax.py
+++ src/minmax.py
@@ -41,7 +41,6 @@ class MinMax(Search):
                 worse_value = value
         return worse_value
 
-
     def minimax_decision(self, board: Board, turn: int, ai_level: int, queue: Queue, max_player: bool):
         max_depth = 4 
         possible_moves = board.get_possible_moves()
@@ -55,43 +54,3 @@ class MinMax(Search):
                 best_value = value
                 best_move = move
         queue.put(best_move)
-
-    
-    def alpha_beta_decision(board, turn, ai_level, queue, max_player):
-        # random move (to modify)
-        queue.put(board.get_possible_moves()[rnd.randint(0, len(board.get_possible_moves()) - 1)])
-
-
-    def max_value_ab(self, board: Board, turn: int, alpha: int, beta: int):
-        if board.check_victory():
-            return -1
-        if turn > 42:
-            return 0
-        possible_moves = board.get_possible_moves()
-        value = float('-inf')
-        for move in possible_moves:
-            updated_board = board.copy()
-            updated_board.add_disk(move, turn % 2 + 1, update_display=False)
-            value = max(value, self.min_value_ab(updated_board, turn + 1, alpha, beta))
-            if value >= beta:
-                return value
-            alpha = max(alpha, value)
-        return value
-
-
-    def min_value_ab(self, board: Board, turn: int, alpha: int, beta: int):
-        if board.check_victory():
-            return 1
-        if turn > 42:
-            return 0
-        possible_moves = board.get_possible_moves()
-        value = float('inf')
-        for move in possible_moves:
-            updated_board = board.copy()
-            updated_board.add_disk(move, turn % 2 + 1, update_display=False)
-            value = min(value, self.max_value_ab(updated_board, turn + 1, alpha, beta))
-            if value <= alpha:
-                return value
-            beta = min(beta, value)
-        return value
-
